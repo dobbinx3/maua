7973c0d3591257ebc37a398a89e7a12f2ccb6251
Dobbinx3 <dobbinx3@gmail.com>
2019-03-07 16:47:09 -0300

    adicionando lambda

--- pae/desenvolvimento_de_sites_responsivos_modernos/exemplos/2018_2/final/js/script.js
+++ pae/desenvolvimento_de_sites_responsivos_modernos/exemplos/2018_2/final/js/script.js
@@ -13,7 +13,29 @@ $(document).ready(function () {
   // Formulario
   $(function () {
     $('#contatoModal .sendButton').on('click', function () {
-        let url = "contact_form/contact_form.php";
+        // let url = "contact_form/contact_form.php";
+
+        // $.ajax({
+        //   type: "POST",
+        //   url: url,
+        //   data: $('#contact-form').serialize(),
+        //   success: function (data)
+        //   {
+        //     let messageAlert = 'alert-' + data.type;
+        //     let messageText = data.message;
+
+        //     let alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+        //     if (messageAlert && messageText) {
+        //         $('#contatoModal').find('.messages').html(alertBox);
+        //         if (messageAlert == "alert-success") {
+        //             $('#contact-form')[0].reset();
+        //         }
+        //     }
+        //   }
+        // });
+
+        // Lambda Version
+        var url = "https://lx4vlicjkb.execute-api.sa-east-1.amazonaws.com/prod/send_email";
 
         $.ajax({
           type: "POST",
@@ -21,16 +43,14 @@ $(document).ready(function () {
           data: $('#contact-form').serialize(),
           success: function (data)
           {
-            let messageAlert = 'alert-' + data.type;
-            let messageText = data.message;
-
-            let alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-            if (messageAlert && messageText) {
-                $('#contatoModal').find('.messages').html(alertBox);
-                if (messageAlert == "alert-success") {
-                    $('#contact-form')[0].reset();
-                }
-            }
+              var alertBox = '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + data.message + '</div>';
+              $('#contatoModal').find('.messages').html(alertBox);
+              $('#contact-form')[0].reset();
+          },
+          error: function (err)
+          {
+              var alertBox = '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + err.responseJSON.message + '</div>';
+              $('#contatoModal').find('.messages').html(alertBox);
           }
         });
     });
